93fad3f4de9349c20b37cb0c89e3072706552db7
Ahmed Abusweireh <82058235+retenuphilistia13@users.noreply.github.com>
2023-08-18 22:51:36 +0300

    Dynamic chess table
    *Resizable table; depending of how many rows and columns given.
    
    )added sublime build to build the project with ease, its primary for windows user

--- chess_main.cpp
+++ chess_main.cpp
@@ -3,13 +3,32 @@
 #include<String>
 
 #include<memory>
-////game headers/////
+ ////game headers/////
 #include <raylib.h>
 
 #define RAYGUI_IMPLEMENTATION
 #include <raygui.h>
 
 
+//////////////Sublime_Build//////////////////
+// {
+//     "cmd": ["gcc", "-o", "$file_base_name", "$file" ,
+//      "-Iraylib-4.5.0_win64_mingw-w64/include","-Lraylib-4.5.0_win64_mingw-w64/lib",
+//      "-lraylib", "-lopengl32", "-lgdi32", "-lwinmm", "-lstdc++",
+//         "-Wno-enum-compare" ],
+
+//     "working_dir": "$file_path",
+//     "selector": "source.cpp",
+//     "shell": true,
+
+//     "variants": [
+//         {
+//             "name": "Run",
+//             "cmd": ["start", "cmd", "/k", "$file_base_name"]
+//         }
+//     ]
+// }
+////////////////////
 
 // Add this line to include the thread library
 using namespace std;
@@ -21,143 +40,158 @@ const Color green = {
    255
 };
 const Color darkGreen = {
-  43,
-    51,
-    24,
+   43,
+   51,
+   24,
    255
 };
 
- int cellSize = 10;
+int cellSize = 10;
 const int cellCount = 12;
 
 const std::string Title = "CHESS ENJOYER";
 
-
 const char * getString(const std::string title) {
-    const char * titleCStr = title.c_str();
-    return titleCStr;
+   const char * titleCStr = title.c_str();
+   return titleCStr;
 }
 
-void drawBoard() {
-    // Check if the animation is up or down based on countBackground value
+class Table {
 
-    // if(!flagPause)
+   int tableRow ;
+   int tableCol ;
+   // Drawing
+   // Calculate the total width and height of the grid
+   float totalWidth;
+   float totalHeight;
 
-    
-    // Drawing
-    // Calculate the total width and height of the grid
-    int totalWidth = 8 * cellSize;
-    int totalHeight = 8 * cellSize;
+   float newCellSize;
+   public:
+      Table() {
+    //standart chess table///
+    tableRow = 8;
+    tableCol = 8;
 
-    // Calculate the starting position to center the grid on the screen
-    int startX = (GetScreenWidth() - totalWidth) / 2;
-    int startY = (GetScreenHeight() - totalHeight) / 2;
+         newCellSize = (float) cellSize;
 
-    // Drawing
-    for (int row = 0; row < 8; ++row) {
-        for (int col = 0; col < 8; ++col) {
-            int x = startX + col * cellSize;
-            int y = startY + row * cellSize;
+         totalWidth = (float) tableCol * newCellSize;
+         totalHeight = (float) tableRow * newCellSize;
+
+      }
+
+   void drawBoard() {
+      // Check if the animation is up or down based on countBackground value
+      // if(!flagPause)
+
+      while (totalWidth > GetScreenWidth() - newCellSize || totalHeight > GetScreenHeight() - newCellSize) {
+
+         newCellSize -= cellSize / 15;
+         totalWidth = tableCol * newCellSize;
+         totalHeight = tableRow * newCellSize;
+
+      }
+
+      // Calculate the starting position to center the grid on the screen
+      float startX = (float)(GetScreenWidth() - totalWidth) / 2.0f;
+      float startY = (float)(GetScreenHeight() - totalHeight) / 2.0f;
+
+      // Drawing
+      for (int row = 0; row < tableRow; ++row) {
+         for (int col = 0; col < tableCol; ++col) {
+            float x = (float) startX + col * newCellSize;
+            float y = (float) startY + row * newCellSize;
             if ((row + col) % 2 == 0) {
-                DrawRectangle(x, y, cellSize, cellSize, darkGreen);
+               DrawRectangle(x, y, newCellSize, newCellSize, BLUE);
             } else {
-                DrawRectangle(x, y, cellSize, cellSize, BLUE);
+               DrawRectangle(x, y, newCellSize, newCellSize, darkGreen);
             }
-        }
-    }
-   //  std::cout << "backgroundState: " << backgroundStateToString(backgroundState) << std::endl;
-
-      //  std::cout<<"countBackground: "<<countBackground<<std::endl;
-
-}
-
-typedef enum UISTATE{
-   MENU,
-   GAME,
-   SETTEING
-} UISTATE;
-
-
-class Button{
-private:
- Rectangle buttonRec;
-
-public:
-float buttonWidth=150;
-float buttonHeight=70;
-
-bool buttonClicked = false;
-
-  string title="start game";
-   Button(float width,float height,float x,float y){
-
-
-   buttonRec = {
-      x,
-      y,
-      width,
-      height
-   };
-
- title="start Game";
-
- update();
+         }
+      }
 
    }
-
-   Button(const string bTitle){
- 
-    buttonRec = {
-      (float) GetScreenWidth() / 2 -(buttonWidth/2.0f),
-      (float) GetScreenHeight() / 2 -(buttonHeight/2.0f),
-      buttonWidth,
-      buttonHeight
-   };
-
- title=bTitle;
-
- update();
-
-}
-
-   void draw(){
- // Calculate text position to center it within the rectangle
-        int textWidth = MeasureText(getString(title), 20);
-        int textHeight = 20;
-        int textX = buttonRec.x + (buttonRec.width - textWidth) / 2;
-        int textY = buttonRec.y + (buttonRec.height - textHeight) / 2;
-        DrawRectangleRounded(buttonRec, 0.2, 0, DARKGRAY);
-        DrawText(getString(title), textX, textY , 20, RAYWHITE);
-   }
-
-   void update(){
-       buttonClicked = GuiButton(buttonRec, getString(title));
-   }
-
-bool isButtonClicked(){
-   return buttonClicked;
-}
-
 
 };
 
+typedef enum UISTATE {
+   MENU,
+   GAME,
+   SETTEING
+}
+UISTATE;
 
+class Button {
+   private: Rectangle buttonRec;
 
+   public: float buttonWidth = 150;
+   float buttonHeight = 70;
+
+   bool buttonClicked = false;
+
+   string title;
+
+   Button(const string Btitle, Vector2 size, Vector2 pos) {
+
+      title = Btitle;
+
+      buttonRec = {
+         pos.x,
+         pos.y,
+         size.x,
+         size.y
+      };
+
+      update();
+
+   }
+
+   Button(const string bTitle) {
+
+      buttonRec = {
+         (float) GetScreenWidth() / 2 - (buttonWidth / 2.0f),
+         (float) GetScreenHeight() / 2 - (buttonHeight / 2.0f),
+         buttonWidth,
+         buttonHeight
+      };
+
+      title = bTitle;
+
+      update();
+
+   }
+
+   void draw() {
+      // Calculate text position to center it within the rectangle
+      int textWidth = MeasureText(getString(title), 20);
+      int textHeight = 20;
+      int textX = buttonRec.x + (buttonRec.width - textWidth) / 2;
+      int textY = buttonRec.y + (buttonRec.height - textHeight) / 2;
+      DrawRectangleRounded(buttonRec, 0.2, 0, DARKGRAY);
+      DrawText(getString(title), textX, textY, 20, RAYWHITE);
+   }
+
+   void update() {
+      buttonClicked = GuiButton(buttonRec, getString(title));
+   }
+
+   bool isButtonClicked() {
+      return buttonClicked;
+   }
+
+};
 
 int main() {
 
    const char * titleCStr = getString(Title);
 
-  const int screenWidth = 800+200;
-    const int screenHeight = 600+200;
+   const int screenWidth = 1000;
+   const int screenHeight = 1000;
 
-    InitWindow(screenWidth, screenHeight, "Chess Enjoyer");
+   InitWindow(screenWidth, screenHeight, "Chess Enjoyer");
 
-    // Calculate the appropriate cell size based on the screen dimensions and cell count
-     cellSize = min(screenWidth / cellCount, screenHeight / cellCount);
+   // Calculate the appropriate cell size based on the screen dimensions and cell count
+   cellSize = min(screenWidth / cellCount, screenHeight / cellCount);
 
-
-std::cout<<"cellSize "<<cellSize;
+   std::cout << "cellSize " << cellSize;
    SetWindowPosition(250, 100);
    // layout_name: controls initialization
    //----------------------------------------------------------------------------------
@@ -165,66 +199,64 @@ std::cout<<"cellSize "<<cellSize;
    //----------------------------------------------------------------------------------
    SetTargetFPS(60);
 
-
-   int textWidth = MeasureText(titleCStr, 40);
+   int textWidth = MeasureText(titleCStr, 40); //game title
    int x = screenWidth / 2 - (textWidth / 2);
 
+   const float buttonWidth = 150;
+   const float buttonHeight = 70;
 
-// float buttonWidth=150;
-// float buttonHeight=70;
-//    // Create a rectangle for the button
-//    Rectangle buttonRec = {
-//       (float) GetScreenWidth() / 2 -(buttonWidth/2.0f),
-//       (float) GetScreenHeight() / 2 -(buttonHeight/2.0f),
-//       buttonWidth,
-//       buttonHeight
-//    };
+   Vector2 bSize;
+   Vector2 bPosition;
 
-// bool buttonClicked = false;
-Button startButton("play game");
-UISTATE ui_state = MENU;
+   bPosition.x = (float) GetScreenWidth() / 2 - (buttonWidth / 2.0f);
+   bPosition.y = (float) GetScreenHeight() / 2 - (buttonHeight / 2.0f);
+
+   bSize.x = buttonWidth;
+   bSize.y = buttonHeight;
+
+   Button startButton("play game", bSize, bPosition);
+
+   UISTATE ui_state = MENU;
+
+   Table table;
    ///game loop////
    while (!WindowShouldClose()) {
 
-// if(!buttonClicked)
-      if(ui_state==MENU)
+      // if(!buttonClicked)
+      if (ui_state == MENU)
          startButton.update();
-  // buttonClicked = GuiButton(buttonRec, "Start game");
+      // buttonClicked = GuiButton(buttonRec, "Start game");
 
       BeginDrawing();
 
       ClearBackground(BLACK);
       // Update
-     
 
       if (IsKeyDown(KEY_Q)) {
          // running = false;
-      } 
-////Reatart game///////
+      }
+      ////Reatart game///////
       else if (IsKeyDown(KEY_R)) {
 
          // running = false;
 
       }
 
-    
-drawBoard();
- 
+      table.drawBoard();
 
-    // Handle button click without drawing the button
-    if (startButton.isButtonClicked()) {
-        startButton.buttonClicked = true;
-        ui_state=GAME;
-        // Your code here to respond to the button click
-    }
+      // Handle button click without drawing the button
+      if (startButton.isButtonClicked()) {
+         startButton.buttonClicked = true;
+         ui_state = GAME;
+         // Your code here to respond to the button click
+      }
 
-    // Draw the button background if it was not clicked
-    if (! startButton.buttonClicked) {
-startButton.draw();
-    }
+      // Draw the button background if it was not clicked
+      if (!startButton.isButtonClicked()) {
+         startButton.draw();
+      }
 
       DrawText(titleCStr, x, 80, 40, WHITE);
-
 
       DrawFPS(10, 10);
       EndDrawing();
@@ -234,4 +266,3 @@ startButton.draw();
    CloseWindow();
    return 0;
 }
-
